47f772e20476f1a9084679eb66a194175ae48158
will-zzy <732696442@qq.com>
2025-10-23 23:15:07 +0000

    done

--- .vscode/debug.sh
+++ .vscode/debug.sh
@@ -2,32 +2,33 @@
 
 # case=Truck
 case=1747834320424
+# case=1748165890960
 root_dir=/home/zzy/data/sa/$case
 # name=sig_GS_R2_speedy_forward
 name=sig_GS_useAnySplatAnchor_12_10
 # name=sig_GS_R2_baseline
 model_dir=$root_dir/exp/$name
-voxel_size=0.01
+voxel_size=0.001
 appearance_dim=16
 update_init_factor=16
 VGGY_PATH=/home/zzy/lib/siggraph_asia/vggt
-densify_grad_threshold=0.0002
+densify_grad_threshold=0.001
 
 
 
 feat_dim=64
-n_offsets=5 # 每个anchor的子高斯数
+n_offsets=10 # 每个anchor的子高斯数
 
 densify_until_iter=5000
-update_from=500
+update_from=100
 densify_from_iter=$update_from
 update_until=$densify_until_iter
-densification_interval=100
+densification_interval=300
 FF_downsample=16 # 对anySplat的点下采样倍数，用于充当anchor
 
 
 #  --eval
-
+rm -r $model_dir/test
 ANY_SPLAT_VGGT_WEIGHTS=$VGGY_PATH python \
     -m debugpy --wait-for-client --listen localhost:5684 \
     train_dash.py -s \
